75a86f4b87b40e8c81d6a65de720cdd45577c5b8
SpencerHerring <herrin348@morrisville.edu>
2019-09-02 21:19:27 -0400

    Ray Casting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,10 +7,18 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+
+    Ray lastRay;
     
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
